9d1e95f9ffc09e722bc8ec53d42b003fc23cc8b6
co2co2 <coco8287@hotmail.com>
2017-11-21 14:10:44 -0500

    kitten pages done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,19 @@
 
 class PagesController < ApplicationController
   def welcome
-    render :welcome
+    @header = "This is the welcome page"
   end
   def about
-    render :about
-  end 
+    @header = "We are ...."
+  end
+  def contest
+    render :contest
+
+  end
+
+  def kitten
+    requested_size = params[:size]
+  @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+  end
 
 end
